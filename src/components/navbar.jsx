040a576382116ebcad7ed06a63f93a806a3ecbde
Yana <ayh916@gmail.com>
2021-09-11 09:07:16 +1000

    set habit-tracker template

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Navbar() {
+    return (<>
+        <div>
+            <img src="" alt="leaf" />
+            <div>Habit Tracker</div>
+            <div className="counter">0</div>
+        </div>
+
+    </>)
+} 
